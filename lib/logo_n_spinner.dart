a745202c588b257205faee3b05005b1fbcae4dc0
pmatatias <matatias97@gmail.com>
2024-05-27 21:06:56 +0700

    test

--- lib/logo_n_spinner.dart
+++ lib/logo_n_spinner.dart
@@ -1,5 +1,1 @@
-/// A widget that displays a logo and a spinner using image assets.
-///
-/// This widget is used to show a logo image and a spinner animation. It can be
-/// used as a loading indicator or as a decorative element in your Flutter app.
 export 'package:logo_n_spinner/src/logo_n_spinner.dart';
